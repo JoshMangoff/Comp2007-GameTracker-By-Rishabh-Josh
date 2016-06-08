8eaa71c6fa468a6813353d5e0960d21e16b41244
Rishabh Batra <batra.rishabh71@gmail.com>
2016-06-07 23:26:57 -0400

    Added Games page

---
+++ Comp2007-GameTracker-By-RishabhAndJosh/Games.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Games" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Games.aspx.cs" Inherits="Comp2007_GameTracker_By_RishabhAndJosh.WebForm2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Welcome to Games page!</h2>
+</asp:Content>
---
+++ Comp2007-GameTracker-By-RishabhAndJosh/Games.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+* @Description: This file is used to track Games statistics
+* @Author:      Rishabh Batra & Josh Mangoff
+* @Date :       June 7th, 2016
+* @Version:     0.1
+*/
+
+namespace Comp2007_GameTracker_By_RishabhAndJosh
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
